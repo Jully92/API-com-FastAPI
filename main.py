5971d091c286fee8ad3eb97c589a5f1c884deb97
Juliana <julianacdo92@gmail.com>
2026-04-16 22:23:26 -0300

    salvando progresso

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
-from fastapi import FastAPI
 import random
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+
+class Estudante (BaseModel):
+    name: str
+    curso: str
+    ativo: bool
+
 #127.0.0.1:8000/
-@app.get("/")
+@app.get ("/helloworld")
 async def root():
-    return {"Menssagem": "Hello World"}
+    return {"message": "Hello World"}
 
 #127.0.0.1:8000/teste1
-@app.get("/teste1")
+@app.get("/funcaoteste")
 async def funcaoteste():
-    return {"Teste": True,"num_aleatorio": random.randint(0, 2000)}
+    return {"teste": True,"num_aleatorio": random.randint(0, 2000)}
+
+@app.post("/estudantes/cadastro")
+async def create_estudante(estudante: Estudante):
+    return estudante
+
+#Atualiza
+@app.put ("/estudantes/update/{id_estudante}")
+async def update_item(id_estudante: int):
+    return id_estudante > 0
+
+#Remove
+@app.delete("/estudantes/delete/{id_estudante}")
+async def delete_item(id_estudante: int):
+    return id_estudante > 0
