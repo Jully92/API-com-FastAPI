e5835be3f6c2cd666b988fa18af6cd95371cb9ea
Juliana <julianacdo92@gmail.com>
2026-04-17 20:13:23 -0300

    Adicionando testes no Docker

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
-
+#Pydantic é um pré requisito para usar o BaseModel
 class Estudante (BaseModel):
     name: str
     curso: str
@@ -13,23 +13,24 @@ class Estudante (BaseModel):
 #127.0.0.1:8000/
 @app.get ("/helloworld")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hellow World"}
 
 #127.0.0.1:8000/teste1
 @app.get("/funcaoteste")
 async def funcaoteste():
     return {"teste": True,"num_aleatorio": random.randint(0, 2000)}
 
+#Cadastrando alunos
 @app.post("/estudantes/cadastro")
 async def create_estudante(estudante: Estudante):
     return estudante
 
-#Atualiza
+#Atualiza algo no sistema (cadastro)
 @app.put ("/estudantes/update/{id_estudante}")
 async def update_item(id_estudante: int):
     return id_estudante > 0
 
-#Remove
+#Remove algo do sistema (cadastro)
 @app.delete("/estudantes/delete/{id_estudante}")
 async def delete_item(id_estudante: int):
     return id_estudante > 0
